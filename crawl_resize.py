8603197aae4efdccfd80d2d1edcab4192c9d745b
phillyai <sonagi.philly@gmail.com>
2017-02-18 03:11:16 +0900

    매개변수로 페이지 지정

--- crawl_resize.py
+++ crawl_resize.py
@@ -2,11 +2,12 @@ from danbooru import pages
 from PIL import Image
 import requests
 from io import BytesIO
+import sys
 
 
-def get(size=(300, 300)) -> (Image, int):
+def get(size=(300, 300), page=1) -> (Image, int):
     ''':returns PIL.Image, 0~2 raiting level. 0 is safe, 1 is questionable, 2 is explicit.'''
-    p = pages(limit=100)
+    p = pages(limit=100, page=page)
     while True:
         post = next(p)
         if 'file_url' in post.keys():
@@ -14,3 +15,6 @@ def get(size=(300, 300)) -> (Image, int):
             img = Image.open(BytesIO(response.content))
             img = img.resize(size, Image.ANTIALIAS)
             yield img, 'sqe'.index(post['rating'])
+
+if __name__ == '__main__':
+    get(page=sys.argv[1])
