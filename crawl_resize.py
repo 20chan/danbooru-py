41a5e424c7adad468b6be8a3a60ca811ad483aed
phillyai <sonagi.philly@gmail.com>
2017-02-18 02:06:13 +0900

    크롤링 후 자동으로 사이즈를 변경하는 무한 제너레이터 추가

---
+++ crawl_resize.py
@@ -0,0 +1,16 @@
+from danbooru import pages
+from PIL import Image
+import requests
+from io import BytesIO
+
+
+def get(size=(300, 300)) -> (Image, int):
+    ''':returns PIL.Image, 0~2 raiting level. 0 is safe, 1 is questionable, 2 is explicit.'''
+    p = pages(limit=100)
+    while True:
+        post = next(p)
+        if 'file_url' in post.keys():
+            response = requests.get('https://danbooru.donmai.us{}'.format(post['file_url']))
+            img = Image.open(BytesIO(response.content))
+            img = img.resize(size, Image.ANTIALIAS)
+            yield img, 'sqe'[post['rating']]
